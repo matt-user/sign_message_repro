ff06b8b070d590a62d2701be14ffc7efbcb6f60b
Matt Auer <mattauer@umich.edu>
2024-03-01 13:57:25 -0600

    change variable name

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,13 +7,12 @@ import base58 from "bs58";
 function App() {
 
   async function signMessage() {
-    const txId = "0x73770495abeca393c0c0507c2f96f8f2ee3589c429683ecf7bf2a7b055962f2b";
-    const u8TxId = uint8arraytools.fromHex(txId.slice(2)); // uint8array tools does not expect the 0x prefix
-    console.log(`u8TxId`, base58.decode("3ozYXfVgcdYqPbtdzfSufrJY8QUtrhPkb3bCbfbN9koy24iwAPbeZWsFg8MX9s75LftJRWU8zMUokmZnK2Y7gQ23"));
-    const naclSig = nacl.sign.detached(u8TxId, base58.decode("3ozYXfVgcdYqPbtdzfSufrJY8QUtrhPkb3bCbfbN9koy24iwAPbeZWsFg8MX9s75LftJRWU8zMUokmZnK2Y7gQ23"));
+    const hexString = "0x73770495abeca393c0c0507c2f96f8f2ee3589c429683ecf7bf2a7b055962f2b";
+    const u8HexString = uint8arraytools.fromHex(hexString.slice(2)); // uint8array tools does not expect the 0x prefix
+    const naclSig = nacl.sign.detached(u8HexString, base58.decode("3ozYXfVgcdYqPbtdzfSufrJY8QUtrhPkb3bCbfbN9koy24iwAPbeZWsFg8MX9s75LftJRWU8zMUokmZnK2Y7gQ23"));
     console.log(`naclSig`, naclSig);
     const provider = window.phantom?.solana;
-    const signedMessage = await provider.signMessage(u8TxId, 'hex');
+    const signedMessage = await provider.signMessage(u8HexString, 'hex');
     console.log(`signedMessage`, signedMessage);
   }
 
